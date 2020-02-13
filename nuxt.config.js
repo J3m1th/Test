d98757d8126441db2a8a921b0035e9bd98238c51
Denys Petryniak <denys.petryniak@gmail.com>
2020-02-13 21:37:01 +0200

    fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,7 @@ module.exports = {
         meta: [
             { charset: 'utf-8' },
             { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            { hid: got 'description', name: 'description', content: 'Nuxt.js project' }
+            { hid: 'description', name: 'description', content: 'Nuxt.js project' }
         ],
         link: [
             { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
